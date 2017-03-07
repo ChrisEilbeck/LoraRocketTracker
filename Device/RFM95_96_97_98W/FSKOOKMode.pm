139af67aac6bcd99d6cd1fbf48b34292c9f692d6
Chris Eilbeck <chris@hyperspace.org.uk>
2017-03-07 14:35:25 +0000

    Now talks to an actual device and reads its frequency

---
+++ Device/RFM95_96_97_98W/FSKOOKMode.pm
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+package Device::RFM95_96_97_98W::FSKOOKMode;
+require Exporter;
+
+our @ISA=qw(Exporter);
+
+our @EXPORT_OK=qw(
+                );
+
+use Device::RFM95_96_97_98W::Constants;
+
+1;
